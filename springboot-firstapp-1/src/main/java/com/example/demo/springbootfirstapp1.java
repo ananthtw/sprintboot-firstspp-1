ede6cb2127fd42133706f90993ad8555d26e0f43
Teju <143856335+ananthtw@users.noreply.github.com>
2023-09-29 16:38:55 -0500

    FIRST COMMIT

---
+++ springboot-firstapp-1/src/main/java/com/example/demo/springbootfirstapp1.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+import org.springframework.boot.SpringApplication;    
+import org.springframework.boot.autoconfigure.SpringBootApplication;   
+@SpringBootApplication 
+public class springbootfirstapp1 {
+	public static void main(String[] args)  
+	{    
+	SpringApplication.run(springbootfirstapp1.class, args);    
+	} 
+}
